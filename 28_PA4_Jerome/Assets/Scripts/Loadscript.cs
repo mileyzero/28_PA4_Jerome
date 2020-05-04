0013047136077bbf3999b1f595d8beb54b8bec58
FrozenHeavenz <chenjingxuevernon@gmail.com>
2020-05-04 16:53:46 +0800

    Updated By Vernon

---
+++ 28_PA4_Jerome/Assets/Scripts/Loadscript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Loadscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadAft1sec", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadAft1sec()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
